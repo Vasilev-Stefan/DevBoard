9098e6995ec5e39d32da42e3a26a5f8e36adb9c8
Vasilev-Stefan <sbasilev@gmail.com>
2026-06-03 23:25:27 +0300

    Add Header component

--- devBoard/src/App.jsx
+++ devBoard/src/App.jsx
@@ -1,9 +1,11 @@
+import Header from './components/Header'
 import './index.css'
 
 function App() {
 
   return (
     <div className='min-h-screen bg-gray-600'>
+      <Header />
     </div>
   )
 }
---
+++ devBoard/src/components/Header.jsx
@@ -0,0 +1,12 @@
+export default function Header() {
+
+    return (
+        <>
+        <div>
+            <p className="text-4xl font-bold text-white">DevBoard</p>
+            {/* Add date here */}
+            <p className="text-xl text-gray-300">Stefan's dev dashboard</p>
+        </div>
+        </>
+    )
+}
